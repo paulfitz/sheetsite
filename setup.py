a271adc468083fca191c425e0f0b20d9c9bbc984
Paul Fitzpatrick <paul.michael.fitzpatrick@gmail.com>
2015-10-12 22:35:26 -0400

    bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(fname, fname2):
 
 setup(
     name = "sheetsite",
-    version = "0.1.2",
+    version = "0.1.3",
     author = "Paul Fitzpatrick",
     author_email = "paul.michael.fitzpatrick@gmail.com",
     description = ("read google sheets, use them for sites"),
