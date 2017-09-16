6ee3dc3b4c2338fbcfedd8a679b20cd5040c3318
Paul Fitzpatrick <paul.michael.fitzpatrick@gmail.com>
2017-09-16 15:22:58 -0400

    give up on supporting not-logged-in gsheet access

--- sheetsite/source/google.py
+++ sheetsite/source/google.py
@@ -1,7 +1,7 @@
 def read_source_google(source):
     from sheetsite.google_spreadsheet import GoogleSpreadsheet
     wb = GoogleSpreadsheet()
-    wb.connect(source.get('credential_file', None))
+    wb.connect(source['credential_file'])
     wb.load_remote(source['key'])
     return wb
 
