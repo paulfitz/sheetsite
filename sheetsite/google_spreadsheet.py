1a0733f08e28e08f7b4e017bbfd08bc0f8f9fafe
Paul Fitzpatrick <paul.michael.fitzpatrick@gmail.com>
2017-09-16 15:20:29 -0400

    remove gspread cruft

--- sheetsite/google_spreadsheet.py
+++ sheetsite/google_spreadsheet.py
@@ -1,9 +1,4 @@
-try:
-    import pygsheets as gspread
-    simple_auth = True
-except ImportError:
-    import gspread
-    simple_auth = False
+import pygsheets
 
 
 class GoogleSpreadsheet(object):
@@ -13,26 +8,10 @@ class GoogleSpreadsheet(object):
         self.workbook = None
 
     def connect(self, credential_file):
-        if simple_auth:
-            self.connection = gspread.authorize(service_file=credential_file)
-            return
-        if credential_file:
-            from oauth2client.service_account import ServiceAccountCredentials
-            scope = ['https://spreadsheets.google.com/feeds']
-            credentials = ServiceAccountCredentials.from_json_keyfile_name(credential_file, scope)
-            self.connection = gspread.authorize(credentials)
-        else:
-            # rely on gspread_public fork
-            self.connection = gspread.public()
+        self.connection = pygsheets.authorize(service_file=credential_file)
 
     def load_remote(self, spreadsheet):
-        try:
-            self.workbook = self.connection.open(spreadsheet)
-        except gspread.exceptions.SpreadsheetNotFound:
-            try:
-                self.workbook = self.connection.open_by_url(spreadsheet)
-            except gspread.exceptions.NoValidUrlKeyFound:
-                self.workbook = self.connection.open_by_key(spreadsheet)
+        self.workbook = self.connection.open_by_key(spreadsheet)
 
     def worksheets(self):
         return self.workbook.worksheets()
